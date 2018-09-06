b476b96b4f6a04b8ad43a743aa2be83746847480
Cedrick GAILLARD <cg@scality.com>
2018-09-06 18:23:18 +0200

    increase total number of colors to 42 (instead of 22)

--- js/functions.js
+++ js/functions.js
@@ -208,7 +208,7 @@ function getShortName(nodeName) {
 function computeRange(data) {
     var ringSizeLimit = new BigNumber("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF", 16);
     var n = data.length;
-    var keysize, cur, prv, lst;
+    var cur, prv, lst;
 
     var x = -1; while(++x < n) {
         if(x == 0) { // First element, we need to calcul the range from the last element in the array
@@ -285,3 +285,29 @@ function writeLegend2(data, rawstats) {
     d3.select("th#members").attr("colspan", colnumbers);
 }
 
+function LightenDarkenColor(col,amt) {
+    var usePound = false;
+    if ( col[0] == "#" ) {
+        col = col.slice(1);
+        usePound = true;
+    }
+
+    var num = parseInt(col,16);
+
+    var r = (num >> 16) + amt;
+
+    if ( r > 255 ) r = 255;
+    else if  (r < 0) r = 0;
+
+    var b = ((num >> 8) & 0x00FF) + amt;
+
+    if ( b > 255 ) b = 255;
+    else if  (b < 0) b = 0;
+
+    var g = (num & 0x0000FF) + amt;
+
+    if ( g > 255 ) g = 255;
+    else if  ( g < 0 ) g = 0;
+
+    return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
+}
